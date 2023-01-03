696f09eb576251651333cfb0e12ed9ad358bf01c
Marvin Clerc <marvin.clerc@edu.devinci.fr>
2023-01-03 12:42:08 +0100

    up to date search bar

--- Axios-search.js
+++ Axios-search.js
@@ -13,14 +13,24 @@ console.log(search)
 
 axios.get('https://api.themoviedb.org/3/search/movie?api_key=a8871525bb27f1c83641251be4509be6&language=en-US&query='+search+'&page=1&include_adult=false')
     .then(function (response) {
-        console.log(response)
         // traiter la réponse de l'API
-        const data = response.data;
-        console.log(data);
+        const movies = response.data.results;
+        movies.forEach(elements){
+            document.write('<h2 class="text-white text-2xl font-Bahn">elements.original_title</h1>')
+
+        }
+
+
+
+
     })
     .catch(function (error) {
         // gérer les erreurs
     });
+
+
+
+
 /*
 button.addEventListener('click', function (event) {
     event.preventDefault(); // empêche l'envoi du formulaire
