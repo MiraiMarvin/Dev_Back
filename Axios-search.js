179c690559e76db03f6ff98f68ce893517d80d25
Marvin Clerc <marvin.clerc@edu.devinci.fr>
2023-01-04 18:07:09 +0100

    update single page

--- Axios-search.js
+++ Axios-search.js
@@ -11,31 +11,59 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const search = urlParams.get('search')
 
-console.log(search)
-
 
 axios.get('https://api.themoviedb.org/3/search/movie?api_key=a8871525bb27f1c83641251be4509be6&language=en-US&query='+search+'&page=1&include_adult=false')
     .then(function (response) {
         // traiter la réponse de l'API
         const movies = response.data.results;
         for (var i = 0; i < movies.length; i++){
-            console.log(movies[i]);
+
 
             var div = document.createElement("div");
             div.className ="search-card";
             document.getElementById("right_search").appendChild(div);
+
+
 
             var title = document.createElement("h2");
             title.className ="title_search";
             title.innerHTML = movies[i].title;
             div.appendChild(title);
 
+            var id = document.createElement("p");
+            id.className = "hideid";
+            id.innerHTML = movies[i].id;
+            div.appendChild(id);
+
             var img = document.createElement("img");
             div.appendChild(img);
             img.className ="image_search";
+
             var moviesimg = 'https://image.tmdb.org/t/p/w500' + movies[i].poster_path;
             img.src = moviesimg;
             img.alt = "affiche film";
+
+            // Sélectionne le bouton
+            var button = document.createElement('button');
+            button.className = "button_search";
+            button.innerHTML = 'Voir';
+            div.appendChild(button);
+
+
+
+            div.addEventListener('click', function() {
+                var data = movies[i].id
+
+                var params = new URLSearchParams();
+                for (var key in data) {
+                    params.append(key, data[key]);
+                }
+                var dataString = params.toString();
+
+                // Redirige vers la page cible en passant les informations dans l'URL
+                window.location.href = 'single_movies.php' + dataString;
+
+            });
 
 
         }
@@ -50,7 +78,19 @@ axios.get('https://api.themoviedb.org/3/search/movie?api_key=a8871525bb27f1c8364
 
 
 
+
+
 /*
+var data = {
+                    id: movies[i].id,
+                    nom: movies[i].title
+                };
+                var params = new URLSearchParams();
+                for (var key in data) {
+                    params.append(key, data[key]);
+                }
+                var dataString = params.toString();
+
 button.addEventListener('click', function (event) {
     event.preventDefault(); // empêche l'envoi du formulaire
 
---
+++ single_search.js
@@ -0,0 +1,8 @@
+
+
+
+
+axios.get('https://api.themoviedb.org/3/search/movie?api_key=a8871525bb27f1c83641251be4509be6&language=en-US&query='+search+'&page=1&include_adult=false')
+    .then(function (response) {
+
+    }
